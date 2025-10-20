ec3f8d5229a629cf1b16dca5b25cc031cd35ce48
CrisKunVN <phukun0404@gmail.com>
2025-10-20 23:11:26 +0700

    create react and vite

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+function App() {
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* { Public routes} */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+
+          {/* { Private routes} */}
+          <Route path="/" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
